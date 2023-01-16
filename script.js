7be91625694c0bdc674447f01c363ea07a54c3a4
MM <maryse.mongeau@cegepoutaouais.qc.ca>
2023-01-16 09:04:44 -0500

    Commencer JavaScript : fonction ajouter, l'item n'est pas inséré au bon endroit.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function ajouter(){
+    var item = document.getElementById("desc").value;
+    document.getElementById("maListe").append(item);
+    alert("L'item est bien ajouté"); // Pop-up
+}
+
 console.log("Bonjour");
