6635e52f34e873d296fc4beef3d972d0e35587f5
Maryse M <Maryse.Mongeau@cegepoutaouais.qc.ca>
2023-01-16 11:44:57 -0500

    Ajouter les prix et mettre à jour le total

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 compteur = 1;
 function ajouter(){
-    var item = document.getElementById("desc").value;
-    nouveauP = document.createElement("p");
-    nouveauP.innerText = item;
-    document.getElementById("maListe").append(nouveauP);
+    let item = document.getElementById("desc").value;
+    let prix = document.getElementById("valeur").value;     //Ajouter le prix
+    let nouveauP = document.createElement("p");             // Créer une balise <p>
+    let nouveauSpan = document.createElement("span");
+    nouveauP.innerText = item + " : ";                              // Écrire dans le paragraphe
+    nouveauSpan.innerText =   prix + "$";
+    document.getElementById("maListe").append(nouveauP);   // Ajouter le paragraphe à la liste
+    nouveauP.append(nouveauSpan);                                   // mettre le span à la fin du p
 
+    // Mettre à jour le total
+    let ancienPrix = document.getElementById("total").innerText;
+    let nouveauPrix = +ancienPrix + +prix;                          // +variable : convertir en numérique
+    document.getElementById("total").innerText = nouveauPrix;
+
+    //Code non expliqué
     nouveauInput = document.createElement("input");
     nouveauInput.setAttribute("value", item);
     nouveauInput.setAttribute("hidden",true);
     nouveauInput.setAttribute("name", "item "+compteur);
     compteur++;
     document.forms[0].append(nouveauInput);
-    alert("L'item est bien ajouté"); // Pop-up
+    //alert("L'item est bien ajouté"); // Pop-up
 }
 
 function ajouterEmail(){
