c34e6f64c620bc0989047a3c336aad12eb77c845
MM <maryse.mongeau@cegepoutaouais.qc.ca>
2023-01-16 10:19:37 -0500

    Continuer JS

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+compteur = 1;
 function ajouter(){
     var item = document.getElementById("desc").value;
-    document.getElementById("maListe").append(item);
+    nouveauP = document.createElement("p");
+    nouveauP.innerText = item;
+    document.getElementById("maListe").append(nouveauP);
+
+    nouveauInput = document.createElement("input");
+    nouveauInput.setAttribute("value", item);
+    nouveauInput.setAttribute("hidden",true);
+    nouveauInput.setAttribute("name", "item "+compteur);
+    compteur++;
+    document.forms[0].append(nouveauInput);
     alert("L'item est bien ajouté"); // Pop-up
+}
+
+function ajouterEmail(){
+    var email = document.getElementById("email").value;
+    document.forms[0].action = "mailto:"+email;
 }
 
 console.log("Bonjour");
