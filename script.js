3668a3f27b919c49aefc6d92b766998ab3582dd8
Maryse M. <Maryse.Mongeau@cegepoutaouais.qc.ca>
2024-01-09 12:00:28 -0500

    v3. Quelques changements dans le script

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 compteur = 4; //Variable globale
 function ajouter(){
-    let item = document.getElementById("desc").value;
-    let prix = document.getElementById("valeur").value;     //Ajouter le prix
-    let nouveauP = document.createElement("p");             // Créer une balise <p>
-    let nouveauSpan = document.createElement("span");
-    nouveauP.innerText = item + " : ";                              // Écrire dans le paragraphe
+    let item = document.getElementById("desc").value;   //Récupérer la description
+    let prix = document.getElementById("valeur").value;     //Récupérer le prix
+    let nouveauP = document.createElement("p");  // Créer une balise <p>
+    let nouveauSpan = document.createElement("span"); // Créer une balise span
+    nouveauP.innerText = item + " : ";     // Écrire dans le paragraphe
     nouveauSpan.innerText =   prix + "$";
+    nouveauP.append(nouveauSpan);         // mettre le span à la fin du p
     document.getElementById("maListe").append(nouveauP);   // Ajouter le paragraphe à la liste
-    nouveauP.append(nouveauSpan);                                   // mettre le span à la fin du p
 
     // Mettre à jour le total
-    let ancienPrix = document.getElementById("total").innerText;
-    let nouveauPrix = +ancienPrix + +prix;                          // +variable : convertir en numérique
-    document.getElementById("total").innerText = nouveauPrix;
+    let total = document.getElementById("total")
+    let ancienPrix = total.innerText;
+    let nouveauPrix = +ancienPrix + +prix;   // +variable : convertir en numérique
+    total.innerText = nouveauPrix;
 
     //Créer des nouveaux input hidden dans le formulaire pour envoyer par courriel
     nouveauInput = document.createElement("input");
